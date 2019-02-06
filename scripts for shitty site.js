36ae29cb4e55fcffe41de73f7135b54eb08b0a91
Austin Williams <williamsa1337@gmail.com>
2019-02-05 20:00:38 -0600

    V

--- scripts for shitty site.js
+++ scripts for shitty site.js
@@ -1,13 +1,3 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-}
-
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0px";
-    document.getElementById("main").style.width = "0px";
-}
-
 function show(nr) {
     document.getElementById("table1").style.display = "none";
     document.getElementById("table2").style.display = "none";
